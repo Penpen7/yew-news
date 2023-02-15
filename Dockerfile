b0dc6a29511120a2d6cebb8fa5cc430e6412dfba
Penpen7 <Raosys@live.jp>
2023-02-16 01:46:36 +0900

    initial commit

---
+++ Dockerfile
@@ -0,0 +1,16 @@
+FROM rust:1.66.0-buster as chef
+RUN cargo install cargo-chef
+WORKDIR /app
+
+FROM chef as planner
+COPY . .
+RUN cargo chef prepare --recipe-path recipe.json
+
+FROM chef as builder
+COPY --from=planner /app/recipe.json recipe.json
+COPY Cargo.* /app/
+RUN rustup target add wasm32-unknown-unknown
+RUN cargo chef cook --recipe-path recipe.json --release
+RUN cargo install --locked trunk wasm-bindgen-cli
+COPY . /app
+RUN trunk build --release --public-url yew-news
---
+++ src/main.rs
@@ -0,0 +1,121 @@
+use yew::prelude::*;
+use yew_router::prelude::*;
+
+#[derive(Clone, Routable, PartialEq)]
+enum Route {
+    #[at("/")]
+    Home,
+    #[at("/article/:id")]
+    Article { id: usize },
+    #[not_found]
+    #[at("/404")]
+    NotFound,
+}
+
+fn switch(route: Route) -> Html {
+    match route {
+        Route::Home => html! { <TemplateHome/>},
+        Route::Article { id } => html! { <TemplateArticle { id }/>},
+        Route::NotFound => html! { <TemplateNotFound />},
+    }
+}
+fn main() {
+    wasm_logger::init(wasm_logger::Config::default());
+    yew::Renderer::<App>::new().render();
+}
+
+#[function_component]
+fn TemplateHome() -> Html {
+    html! {
+        <>
+            <Header />
+        </>
+    }
+}
+
+#[function_component]
+fn TemplateNotFound() -> Html {
+    html! {
+        <>
+            <Header />
+            {"ページが見つかりません。"}
+        </>
+    }
+}
+
+struct article {
+    title: String,
+    text: String,
+}
+
+#[function_component]
+fn TemplateArticle(props: &TemplateArticleProps) -> Html {
+    let articles = vec![
+        article {
+            title: "Rust".to_string(),
+            text: "hello".to_string(),
+        },
+        article {
+            title: "hello".to_string(),
+            text: "hello".to_string(),
+        },
+    ];
+    match articles.get(props.id) {
+        Some(v) => html! {
+            <>
+                <Header />
+                <ArticleText text={v.text.clone()}/>
+                <GoodButton />
+            </>
+        },
+        _ => html! {
+            <Redirect<Route> to={Route::NotFound}/>
+        },
+    }
+}
+
+#[function_component]
+fn App() -> Html {
+    html! {
+        <BrowserRouter>
+            <Switch<Route> render={switch} />
+        </BrowserRouter>
+    }
+}
+
+#[function_component]
+fn Header() -> Html {
+    html! { <h1>{ "Penpen7ニュース" }</h1> }
+}
+
+#[derive(Properties, PartialEq, Clone)]
+struct ArticleTextProps {
+    text: String,
+}
+
+#[derive(Properties, PartialEq, Clone)]
+struct TemplateArticleProps {
+    id: usize,
+}
+
+#[function_component]
+fn GoodButton() -> Html {
+    let good = use_state(|| 0);
+    let on_good_button_click = {
+        let good = good.clone();
+        move |_| {
+            good.set(*good + 1);
+        }
+    };
+    html! {
+      <>
+        <div>{*good}</div>
+        <button onclick={on_good_button_click}>{"いいね"}</button>
+      </>
+    }
+}
+
+#[function_component]
+fn ArticleText(props: &ArticleTextProps) -> Html {
+    html! { <p> { props.text.clone() } </p> }
+}
